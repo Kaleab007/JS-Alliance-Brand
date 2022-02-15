43baf5f5bb4c7e0466943e7080d695d27a0f4fae
Kaleab007 <abatenehkaleab@gmail.com>
2022-02-14 18:39:14 -0800

    done

--- DivByZero_dream_pulse/js/ca-scripts.js
+++ DivByZero_dream_pulse/js/ca-scripts.js
@@ -2,15 +2,31 @@ var customerName = "Alliance  ";
 
 // <h1 id="tm-header" class="tm-brand text-uppercase"></h1> <!--todo: Add Customer Name-->
 function addCustomerName() {
+    let header = document.getElementById('tm-header');
+    header.textContent= customerName;
+    function () {
+        let header = document.getElementById('tm-header');
+        header.textContent= customerName;
+
+        function addCustomerName() {
+            let header = document.getElementById('tm-header');
+            header.textContent= customerName;
+        
 
 };
 
 function toggleColors() {
+    let elementOld = document.querySelectorAll('.tm-white-rect');
+    for (i=0; i<elementOld.length; ++i){
+        elementOld[i].classList.add('tm-blue-rect');
+        elementOld[i].classList.remove('tm-white-rect');
+    }
 
 };
 
 //<ul class="nav flex-row tm-social-links">  <!--todo: Hide social links-->
 function hideElement() {
+    let socialLinksElement = document.getElementById  ('social-links') socialLinksElement.style.display= 'none';
 
 };
 
